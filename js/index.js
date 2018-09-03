9f2a25d03eee6f1f5623b9253ed0b4f7351161f6
liufugang <15753936809@163.com>
2018-09-03 18:53:58 +0800

    learn-vue

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var  app=new Vue({
+    el:"#app",
+    data:{
+        matho:88,
+        yuwen:99
+    },
+    computed:{
+        sum:function(){
+            return this.matho+this.yuwen
+        }
+    }
+})
